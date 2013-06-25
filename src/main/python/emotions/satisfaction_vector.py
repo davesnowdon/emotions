0c8723062216d890f5712dd7d787c4618c7463b1
Dave Snowdon <dave@davesnowdon.com>
2013-06-25 08:41:46 +0100

    use named tuples for control points and control point averages. Fixed calculation of average value

--- src/main/python/emotions/satisfaction_vector.py
+++ src/main/python/emotions/satisfaction_vector.py
@@ -1,10 +1,16 @@
 '''
 Created on 8 Jun 2013
 
-@author: dsnowdon
+@author: davesnowdon
 '''
 
 import math
+import collections
+
+ControlPoint = collections.namedtuple('ControlPoint', ['valence', 'arousal', 'thresholds'])
+
+ControlPointAverage = collections.namedtuple('ControlPointAverage', ['valence', 'arousal', 'average'])
+
 
 FLOAT_CMP_ACCURACY=0.00000001
 
@@ -18,15 +24,29 @@ def expression_vector_difference(ev, sv):
             difference[mid] = 0
     return difference
 
-def expression_vector_average(ev):
-    return sum(ev.values()) / len(ev)
+def count_non_zero(thresholds):
+    count = 0
+    for mid in thresholds.keys():
+        if not feq(thresholds[mid], 0):
+            count = count + 1
+    return count
+
+def expression_vector_average(ev, num_significant_values=None):
+    count = num_significant_values if num_significant_values else len(ev)
+    if count:    
+        return sum(ev.values()) / count
+    else:
+        return 0
 
 def control_point_averages(control_points, sv):
     cp_averages = []
     for cp in control_points:
-        difference = expression_vector_difference(cp[2], sv)
-        average = expression_vector_average(difference)
-        cp_averages.append((cp[0], cp[1], average))
+        difference = expression_vector_difference(cp.thresholds, sv)
+        average = expression_vector_average(difference,
+                                            count_non_zero(cp.thresholds))
+        cp_averages.append(ControlPointAverage(valence=cp.valence,
+                                               arousal=cp.arousal,
+                                               average=average))
     return cp_averages
 
 def express():
--- src/test/python/emotions_tests/test_satisfaction_vector.py
+++ src/test/python/emotions_tests/test_satisfaction_vector.py
@@ -1,12 +1,12 @@
 '''
 Created on 8 Jun 2013
 
-@author: dns
+@author: davesnowdon
 '''
 import unittest
 
 import emotions.satisfaction_vector as satisfaction_vector
-from emotions.satisfaction_vector import feq
+from emotions.satisfaction_vector import feq, ControlPoint, ControlPointAverage
 
 class TestSatisfactionVector(unittest.TestCase):
 
@@ -25,39 +25,53 @@ class TestSatisfactionVector(unittest.TestCase):
 
     def test_get_averages_for_all_control_points(self):
         sv = { 1: 0.5, 2: 0.7 }
-        control_points = [(1, 1, { 1: 1.0, 2: 1.0 }),
-                          (-1, 0, { 1: 0.5, 2: 0.5 })]
-        expected = [(1, 1, 0.4), (-1, 0, -0.1)]
+        control_points = [ ControlPoint(-1,  1, { 1: 1.0, 2: 1.0 }),
+                           ControlPoint( 0,  1, { 1: 0.5, 2: 0.5 }),
+                           ControlPoint( 1,  1, { 1: 1,   2: 1 }),
+                           ControlPoint(-1,  0, { 1: 1,   2: 1 }),
+                           ControlPoint( 0,  0, { 1: 0,   2: 0 }),
+                           ControlPoint( 1,  0, { 1: 0,   2: 0.5 }),
+                           ControlPoint(-1, -1, { 1: 0.5, 2: 0 }),
+                           ControlPoint( 0, -1, { 1: 0.5, 2: 0.5 }),
+                           ControlPoint( 1, -1, { 1: 0.5, 2: 0.5 }) ]
+        expected = [ ControlPointAverage(-1, 1, 0.4),
+                     ControlPointAverage( 0, 1, -0.1),
+                     ControlPointAverage( 1, 1, 0.4),
+                     ControlPointAverage(-1, 0, 0.4),
+                     ControlPointAverage( 0, 0, 0),
+                     ControlPointAverage( 1, 0, -0.2),
+                     ControlPointAverage(-1, 1, 0),
+                     ControlPointAverage( 0, 1, -0.1),
+                     ControlPointAverage( 1, 1, -0.1) ]
+        
         result = satisfaction_vector.control_point_averages(control_points, sv)
         print "cp average result = {}".format(result)
+        self.assertEqual(len(control_points), len(result), 
+                         "Should get one result for each control point")
         self.assertTrue(self.cp_equal(expected, result), 
                         "expected control point averages {}".format(expected))
         
+        
+    def test_count_non_zero(self):
+        self.assertEqual(1, satisfaction_vector.count_non_zero({1 : 1}), 
+                         "count dict of length 1 is 1")
+        self.assertEqual(0, satisfaction_vector.count_non_zero({1 : 0, 2: 0, 3: 0}), 
+                         "count dict of zero values is 0")
+        self.assertEqual(2, satisfaction_vector.count_non_zero({1 : 1, 2 : 2}), 
+                         "count dict of 2 values is 2")
+        
 #    def test_valence_arousal_from_satisfaction_vector(self):
-#        motivation_id1 = 1
-#        motivation_id2 = 2
-#        sv = { motivation_id1 : 0.1,
-#             motivation_id2 : 0.5 }
-#        control_points = self.make_control_points_with_zeros()
-#        (v, a) = satisfaction_vector.express(sv, control_points)
-#        
-    
-    def cp_equal(self, cps1, cps2):        
+#        self.fail("not implemented")
+      
+    def cp_equal(self, cps1, cps2):
+        if not len(cps1) == len(cps2):
+            return False
+        
         for (v1, v2) in zip(cps1, cps2):
             if not feq(v1[2], v2[2]):
                 return False;
         return True
         
-    def make_control_points_with_zeros(self):
-        return [ (-1,  1, {}),
-                 (0,   1, {}),
-                 (1,   1, {}),
-                 (-1,  0, {}),
-                 (0,   0, {}),
-                 (1,   0, {}),
-                 (-1, -1, {}),
-                 (0,  -1, {}),
-                 (1,  -1, {})]
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
